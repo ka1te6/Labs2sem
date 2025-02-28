7381c3292e783f3c42620f4af49784ca5ff97f3d
ka1te6 <a.andrey2006@yandex.ru>
2025-03-01 00:49:21 +0300

    Добвлен метод исполнения программы черещ массив

--- laba1/main.cpp
+++ laba1/main.cpp
@@ -1,39 +1,64 @@
-#include "Students.hpp"
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include "Students.hpp"
+
+
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    
-    std::ifstream inputFile("students.txt");
-    std::vector<Student> students;
+	setlocale(LC_ALL, "Russian");
+
+	const int MAX_STUDENTS = 100;
+
+	std::ifstream inputFile("students.txt");
+	Student* students = new Student[MAX_STUDENTS];
+	int studentCount = 0;
+
+	std::vector<Student> studentsVector;
+
+	std::string line;
+	while (std::getline(inputFile, line)) {
+		std::istringstream iss(line);
+		std::string name;
+		std::vector<int> grades;
+		iss >> std::ws;
+		std::getline(iss, name, ',');
+		int grade;
+		while (iss >> grade) {
+			grades.push_back(grade);
+		}
+
+		if (studentCount < MAX_STUDENTS) {
+			students[studentCount] = Student(name, grades);
+			studentCount++;
+		}
+
+		studentsVector.emplace_back(name, grades);
+	}
+
+	inputFile.close();
+
+	std::ofstream outputFile("stepuha_students.txt");
 
 
-    std::string line;
-    while (std::getline(inputFile, line)) {
-        std::istringstream iss(line);
-        std::string name;
-        std::vector<int> grades;
-        iss >> std::ws;
-        std::getline(iss, name, ','); 
-        int grade;
-        while (iss >> grade) {
-            grades.push_back(grade);
-        }
-        students.emplace_back(name, grades);
-    }
+	outputFile << "=== Проверка студентов (Массив) ===\n";
+	for (int i = 0; i < studentCount; i++) {
+		if (students[i].nalichie_stepuhi()) {
+			students[i].print();
+			outputFile << "Стипендия одобрена для " << students[i].getName() << ".\n";
+		}
+	}
 
-    inputFile.close();
+	outputFile << "\n=== Проверка студентов (Вектор) ===\n";
+	for (const auto& student : studentsVector) {
+		if (student.nalichie_stepuhi()) {
+			student.print();
+			outputFile << "Стипендия одобрена для " << student.getName() << ".\n";
+		}
+	}
 
-    std::ofstream outputFile("stepuha_students.txt");
-    for (const auto& student : students) {
-        if (student.nalichie_stepuhi()) {
-            student.print();
-            outputFile << "Стипендия одобрена для " << student.getName() << ".\n";
-        }
-    }
-
-    outputFile.close();
-    return 0;
+	outputFile.close();
+	delete[] students;
+	return 0;
 }
